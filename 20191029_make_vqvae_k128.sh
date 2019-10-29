3709af61e6bf5d488c7d506833abd8b21b76cb88
gentaman <gentaman01@gmail.com>
2019-10-30 03:07:45 +0900

    add batch process

---
+++ 20191029_make_vqvae_k128.sh
@@ -0,0 +1,30 @@
+
+
+out_prefix='20191029_transpose_vqvae'
+
+
+k[0]=128
+init_model[0]=$HOME'/data2/20191028_vqvae_init/models/init/init.model.pt'
+init_predictor[0]=$HOME'/data2/20191028_vqvae_init/models/init/init.predictor.pt'
+
+for ((j=0; j < 1; j++))
+do
+    i=1
+    output_dir[$i]='mnist_im64_gap_k'${k[$j]}'_e100_no_recon'
+    args[$i]='--recon_coeff 0.0'
+    i=$((i+1))
+    output_dir[$i]='mnist_im64_gap_k'${k[$j]}'_e100_no_pred'
+    args[$i]='--gamma 0.0'
+    i=$((i+1))
+    output_dir[$i]='mnist_im64_gap_k'${k[$j]}'_e100'
+    args[$i]=''
+    i=$((i+1))
+    max=$i
+
+    # ls ${init_model[0]}
+    for ((i=1; i < $max; i++))
+    do
+        echo ${output_dir[$i]}
+        python vqvae_predict.py --data-folder ~/data2/dataset/ --dataset mnist --image-size 64 --hidden-size 256 --num-epochs 100 --root ~/data2/${out_prefix}_vqvae/ --output-folder ${output_dir[$i]} --gap --k $k --device cuda --batch-size 256 --resblock-transpose ${args[$i]} --predictor ${init_predictor[$j]} --model ${init_model[$j]}
+    done
+done
